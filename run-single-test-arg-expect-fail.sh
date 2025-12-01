2c69c9beb8d9d48f82eb9c6f1f24d7d99b861089
offtkp <parisoplop@gmail.com>
2025-12-01 18:56:09 +0200

    .

---
+++ run-single-test-arg-expect-fail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FELIX=$1
+DIR=$2
+TEST=$3
+SUCCESS_RET=$4
+
+$1 $2 $3 1> /dev/null 2> /dev/null
+RET=$?
+
+if [ $RET -eq $SUCCESS_RET ]; then
+    echo "Test $TEST failed with $RET."
+    exit 1
+fi
+
+echo "Test $TEST succeeded."
+exit 0
