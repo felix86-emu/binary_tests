c9746572da8cbd2f3a78b3ca1b06587ed96adf28
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-01-19 17:04:04 +0200

    .

--- run-tests.sh
+++ run-tests.sh
@@ -19,45 +19,46 @@ if [ ! -x "$FELIX" ]; then
     exit 1
 fi
 
-xargs -P$(nproc) -I{} sh -c "./run-single-test.sh $FELIX $DIR/fex-gcc-target-tests-bins/ {} 0" < gcc_tests_must_pass.txt
-RET=$?
-if [ $RET -ne 0 ]; then
-    echo "Failed some tests"
-    exit 1
-fi
-
-xargs -P$(nproc) -I{} sh -c "./run-single-test-expect-fail.sh $FELIX $DIR/fex-gcc-target-tests-bins/ {} 0" < gcc_tests_known_failure.txt
-RET=$?
-if [ $RET -ne 0 ]; then
-    echo "Failed some tests"
-    exit 1
-fi
-
-xargs -P$(nproc) -I{} sh -c "./run-single-test.sh $FELIX $DIR/valgrind-tests-bins/ {} 0" < valgrind_tests_must_pass.txt
-RET=$?
-if [ $RET -ne 0 ]; then
-    echo "Failed some tests"
-    exit 1
-fi
-
-xargs -P$(nproc) -I{} sh -c "./run-single-test-expect-fail.sh $FELIX $DIR/valgrind-tests-bins/ {} 0" < valgrind_tests_known_failure.txt
-RET=$?
-if [ $RET -ne 0 ]; then
-    echo "Failed some tests"
-    exit 1
-fi
-
 current_dir=$(pwd)
+
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test.sh $FELIX $DIR/fex-gcc-target-tests-bins/ {} 0" < "$current_dir/gcc_tests_must_pass.txt"
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "Failed some tests"
+    exit 1
+fi
+
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test-expect-fail.sh $FELIX $DIR/fex-gcc-target-tests-bins/ {} 0" < "$current_dir/gcc_tests_known_failure.txt"
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "Failed some tests"
+    exit 1
+fi
+
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test.sh $FELIX $DIR/valgrind-tests-bins/ {} 0" < "$current_dir/valgrind_tests_must_pass.txt"
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "Failed some tests"
+    exit 1
+fi
+
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test-expect-fail.sh $FELIX $DIR/valgrind-tests-bins/ {} 0" < "$current_dir/valgrind_tests_known_failure.txt"
+RET=$?
+if [ $RET -ne 0 ]; then
+    echo "Failed some tests"
+    exit 1
+fi
+
 cd "$DIR/libuv-tests"
 
-xargs -P$(nproc) -I{} sh -c "./run-single-test-arg.sh $FELIX $DIR/libuv-tests/uv_run_tests_a {} 0" < "$current_dir/libuv_tests_must_pass.txt"
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test-arg.sh $FELIX $DIR/libuv-tests/uv_run_tests_a {} 0" < "$current_dir/libuv_tests_must_pass.txt"
 RET=$?
 if [ $RET -ne 0 ]; then
     echo "Failed some tests"
     exit 1
 fi
 
-xargs -P$(nproc) -I{} sh -c "./run-single-test-arg-expect-fail.sh $FELIX $DIR/libuv-tests/uv_run_tests_a {} 0" < "$current_dir/libuv_tests_known_failures.txt"
+xargs -P$(nproc) -I{} sh -c "$current_dir/run-single-test-arg-expect-fail.sh $FELIX $DIR/libuv-tests/uv_run_tests_a {} 0" < "$current_dir/libuv_tests_known_failures.txt"
 RET=$?
 if [ $RET -ne 0 ]; then
     echo "Failed some tests"
