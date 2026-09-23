84825208036b766cbba74c975a56aa854c5e70e6
Paris Oplopoios <21157395+OFFTKP@users.noreply.github.com>
2026-09-23 19:08:05 +0300

    Use file for output

--- run-single-test.sh
+++ run-single-test.sh
@@ -6,16 +6,19 @@ TEST=$3
 SUCCESS_RET=$4
 
 export TEST_TMPDIR=$(mktemp -d)
-OUTPUT=$($FELIX $DIR/$TEST 2>&1)
+OUTPUT_FILE="$TEST_TMPDIR.out"
+$FELIX $DIR/$TEST > "$OUTPUT_FILE" 2>&1
 RET=$?
 rm -rf "$TEST_TMPDIR"
 
 if [ $RET -ne $SUCCESS_RET ]; then
     echo "Test $TEST failed with $RET."
     echo "Output:"
-    echo "$OUTPUT"
+    cat "$OUTPUT_FILE"
+    rm -f "$OUTPUT_FILE"
     exit 1
 fi
 
+rm -f "$OUTPUT_FILE"
 echo "Test $TEST succeeded."
 exit 0
